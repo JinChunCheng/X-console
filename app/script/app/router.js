273cb2956571f4ec4c250cc3d39c551cc447f71e
靳春城 <jinchuncheng@iboxpay.com>
2016-08-08 17:38:50 +0800

    新增

--- app/script/app/router.js
+++ app/script/app/router.js
@@ -7,7 +7,6 @@ define(['app', 'lazy-load'], function(app, lazyLoad) {
                 .state('dashboard', lazyLoad.config('', 'view/dashboard/dashboard.html', 'controller/dashboard/dashboard', { directives: [], services: ['service/dashboard'], filters: [] }))
                 .state('404', lazyLoad.config('/404', 'view/shared/404.html', '', { directives: [], services: [], filters: [] }))
                 .state('login', lazyLoad.config('/login', 'view/login/login.html', 'controller/login/login', { directives: [], services: [], filters: [] }))
-                //.state('borrower', lazyLoad.config('/borrower', '', 'controller/borrower/borrower', { directives: [], services: [], filters: [] }, true))
                 .state('borrower-list', lazyLoad.config('/borrower/list', '/view/borrower/list.html', 'controller/borrower/list', { directives: [], services: [], filters: [] }))
                 .state('borrower-repayments', lazyLoad.config('/borrower/repayments', '/view/borrower/repayments.html', 'controller/borrower/repayments', { directives: [], services: [], filters: [] }))
                 .state('investor-investorList', lazyLoad.config('/investor/investorList', '/view/investor/investorList.html', 'controller/investor/investorList', { directives: [], services: [], filters: [] }))
@@ -15,6 +14,19 @@ define(['app', 'lazy-load'], function(app, lazyLoad) {
                 .state('investor-newTender', lazyLoad.config('/investor/newTender', '/view/investor/newTender.html', 'controller/investor/newTender', { directives: [], services: [], filters: [] }))
                 .state('investor-tenderList', lazyLoad.config('/investor/tenderList', '/view/investor/tenderList.html', 'controller/investor/tenderList', { directives: [], services: [], filters: [] }))
                 .state('investor-investmentList', lazyLoad.config('/investor/investmentList', '/view/investor/investmentList.html', 'controller/investor/investmentList', { directives: [], services: [], filters: [] }))
+                .state('projectManagement-projectList', lazyLoad.config('/projectManagement/projectList', '/view/projectManagement/projectList.html', 'controller/projectManagement/projectList', { directives: [], services: [], filters: [] }))
+                .state('projectManagement-newProject', lazyLoad.config('/projectManagement/newProject', '/view/projectManagement/newProject.html', 'controller/projectManagement/newProject', { directives: [], services: [], filters: [] }))
+                .state('projectManagement-releaseCheck', lazyLoad.config('/projectManagement/releaseCheck', '/view/projectManagement/releaseCheck.html', 'controller/projectManagement/releaseCheck', { directives: [], services: [], filters: [] }))
+                .state('projectManagement-endTenderCheck', lazyLoad.config('/projectManagement/endTenderCheck', '/view/projectManagement/endTenderCheck.html', 'controller/projectManagement/endTenderCheck', { directives: [], services: [], filters: [] }))
+                .state('projectManagement-repaymentList', lazyLoad.config('/projectManagement/repaymentList', '/view/projectManagement/repaymentList.html', 'controller/projectManagement/repaymentList', { directives: [], services: [], filters: [] }))
+                .state('fundManagement-chargeList', lazyLoad.config('/fundManagement/chargeList', '/view/fundManagement/chargeList.html', 'controller/fundManagement/chargeList', { directives: [], services: [], filters: [] }))
+                .state('fundManagement-withdrawCheck', lazyLoad.config('/fundManagement/withdrawCheck', '/view/fundManagement/withdrawCheck.html', 'controller/fundManagement/withdrawCheck', { directives: [], services: [], filters: [] }))
+                .state('fundManagement-withdrawList', lazyLoad.config('/fundManagement/withdrawList', '/view/fundManagement/withdrawList.html', 'controller/fundManagement/withdrawList', { directives: [], services: [], filters: [] }))
+                .state('fundManagement-ratePreserve', lazyLoad.config('/fundManagement/ratePreserve', '/view/fundManagement/ratePreserve.html', 'controller/fundManagement/ratePreserve', { directives: [], services: [], filters: [] }))
+                .state('fundManagement-fallbackCheck', lazyLoad.config('/fundManagement/fallbackCheck', '/view/fundManagement/fallbackCheck.html', 'controller/fundManagement/ratePreserve', { directives: [], services: [], filters: [] }))
+                .state('fundManagement-chargeQuery', lazyLoad.config('/fundManagement/chargeQuery', '/view/fundManagement/chargeQuery.html', 'controller/fundManagement/chargeQuery', { directives: [], services: [], filters: [] }))
+                .state('fundManagement-withdrawQuery', lazyLoad.config('/fundManagement/withdrawQuery', '/view/fundManagement/withdrawQuery.html', 'controller/fundManagement/withdrawQuery', { directives: [], services: [], filters: [] }))
+                .state('fundAccount-fundAccountList', lazyLoad.config('/fundAccount/fundAccountList', '/view/fundAccount/fundAccountList.html', 'controller/fundAccount/fundAccountList', { directives: [], services: [], filters: [] }))
                 
             $locationProvider.html5Mode(true);
         }
---
+++ app/script/controller/projectManagement/releaseCheck.js
@@ -0,0 +1,261 @@
+define([], function() {
+    return ['$scope', '$http', '$timeout', '$modal', 'applicationService', function($scope, $http, $timeout, $modal, applicationService) {
+
+        /**
+         * the default search condition
+         * @type {Object}
+         */
+        var defaultCondition = {
+            sorting: 'update_time desc',
+            pageNum: 1,
+            pageSize: 10
+        };
+
+        $scope.listView = {
+            condition: angular.copy(defaultCondition),
+            table: null
+        };
+
+        /**
+         * do something after view loaded
+         * @param  {string}     event type                       
+         * @param  {function}   callback function
+         */
+        $scope.$on('$viewContentLoaded', function() {
+            $scope.listView.table = $('#releaseCheckTable');
+        });
+
+
+        var getData = function(params) {
+            $http({
+                    url: 'script/data/repaymentList.json',
+                    type: 'get'
+                })
+                .success(function(data) {
+                   var tm = $timeout(function() {
+                        //给每一条数据随机加上一个1-100的ID
+                        data.forEach(function(item) {
+                            item.id = parseInt(Math.random() * 100);
+                            item.name = "jjaj";
+
+                        });
+                        //随机排序
+                        data.sort(function(a, b) {
+                            return Math.random() > .5 ? -1 : 1;
+                        });
+                        //bootstrap-table加载成功后将数据填充进去
+                        params.success({
+                            total: 98,
+                            rows: data
+                        });
+                    }, 200);
+                    // $timeout.cancel(tm);
+                })
+                .error(function(err) {
+
+                });
+        };
+
+        (function init() {
+
+            $scope.bsReleaseCheckTableControl = {
+                options: {
+                    //data: rows,
+                    // rowStyle: function(row, index) {
+                    //     return { classes: 'none' };
+                    // },
+                    // fixedColumns: true,
+                    // fixedNumber: 2,
+                    cache: false,
+                    height: 840,
+                    //striped: true,
+                    pagination: true,
+                    pageSize: 10,
+                    pageList: "[10, 25, 50, 100, 200]",
+                    ajax: getData,
+                    //autoLoad: true,
+                    onPageChange: pageChange,
+                    sidePagination: "server",
+                    //search: true,
+                    //showColumns: true,
+                    //showRefresh: false,
+                    //minimumCountColumns: 2,
+                    //clickToSelect: false,
+                    //showToggle: true,
+                    //maintainSelected: true,
+                    columns: [{
+                        field: 'state',
+                        checkbox: true,
+                        align: 'center',
+                        valign: 'middle'
+                    }, {
+                        field: 'id',
+                        title: '编号',
+                        align: 'center',
+                        valign: 'middle',
+                        sortable: true
+                    }, {
+                        field: 'name',
+                        title: '登录名',
+                        align: 'center',
+                        valign: 'middle',
+                        sortable: true
+                    }, {
+                        field: 'workspace',
+                        title: '真实姓名',
+                        align: 'left',
+                        valign: 'top',
+                        sortable: true
+                    }, {
+                        field: 'workspace2',
+                        title: '身份证号码',
+                        align: 'left',
+                        valign: 'top',
+                        sortable: true
+                    }, {
+                        field: 'workspace3',
+                        title: '手机号',
+                        align: 'left',
+                        valign: 'top',
+                        sortable: true
+                    }, {
+                        field: 'workspace4',
+                        title: '固话',
+                        align: 'left',
+                        valign: 'top',
+                        sortable: true
+                    }, {
+                        field: 'workspace5',
+                        title: '状态',
+                        align: 'left',
+                        valign: 'top',
+                        sortable: true
+                    }, {
+                        field: 'workspace6',
+                        title: '理财客户经理编号',
+                        align: 'left',
+                        valign: 'top',
+                        sortable: true
+                    }, {
+                        field: 'workspace7',
+                        title: '理财客户经理代码',
+                        align: 'left',
+                        valign: 'top',
+                        sortable: true
+                    }, {
+                        field: 'workspace8',
+                        title: '理财客户经理姓名',
+                        align: 'left',
+                        valign: 'top',
+                        sortable: true
+                    }, {
+                        field: 'workspace9',
+                        title: '理财渠道代码',
+                        align: 'left',
+                        valign: 'top',
+                        sortable: true
+                    }, {
+                        field: 'workspace10',
+                        title: '理财渠道名称',
+                        align: 'left',
+                        valign: 'top',
+                        sortable: true
+                    }, {
+                        field: 'workspace10',
+                        title: '注册类型',
+                        align: 'left',
+                        valign: 'top',
+                        sortable: true
+                    }, {
+                        field: 'workspace10',
+                        title: '是否本公司员工',
+                        align: 'left',
+                        valign: 'top',
+                        sortable: true
+                    }, {
+                        field: 'workspace10',
+                        title: '邮编',
+                        align: 'left',
+                        valign: 'top',
+                        sortable: true
+                    }, {
+                        field: 'workspace10',
+                        title: '地址',
+                        align: 'left',
+                        valign: 'top',
+                        sortable: true
+                    }, {
+                        field: 'workspace10',
+                        title: '是否新手',
+                        align: 'left',
+                        valign: 'top',
+                        sortable: true
+                    }, {
+                        field: 'workspace10',
+                        title: '试投金状态',
+                        align: 'left',
+                        valign: 'top',
+                        sortable: true
+                    }, {
+                        field: 'flag',
+                        title: '操作',
+                        align: 'center',
+                        valign: 'middle',
+                        clickToSelect: false,
+                        formatter: flagFormatter,
+                        events: {
+                            'click .btn': function(e, value, row, index) {
+                                var text = "确定删除此记录？";
+                                // var text = JSON.stringify($scope.listView.table.bootstrapTable('getAllSelections'));
+                                $modal.open({
+                                    templateUrl: 'view/shared/confirm.html',
+                                    size: 'sm',
+                                    // backdrop: true,
+                                    controller: function($scope, $modalInstance) {
+                                        $scope.confirmData = {
+                                            text: text,
+                                            processing: false
+                                        };
+                                        $scope.cancel = function() {
+                                            $modalInstance.dismiss();
+                                            return false;
+                                        };
+
+                                        $scope.ok = function() {
+                                            delUser(item.id, $scope, $modalInstance);
+                                            return true;
+                                        };
+                                    }
+                                });
+
+                            }
+                        }
+                    }]
+                }
+            };
+
+            function flagFormatter(value, row, index) {
+                return '<button class="btn btn-sm btn-danger" ng-click="del()"><i class="fa fa-remove"></i></button>';
+            }
+
+        })();
+
+        $scope.del = function() {
+            console.log('del');
+        };
+
+        $scope.search = function() {
+            $scope.listView.table.bootstrapTable('refresh');
+            console.log('aaa');
+        };
+
+        $scope.reset = function() {
+            $scope.listView.condition = angular.copy(defaultCondition);
+            console.log('aaa');
+        };
+
+        var pageChange = function(num, size) {
+            console.log(num + ' - ' + size);
+        };
+    }];
+});
