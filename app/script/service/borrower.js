d14dc2e5c5aecd040c96560566361ea2eab7bcbb
尤海洋 <youhaiyang@iboxpay.com>
2016-08-10 10:41:23 +0800

    modify file name

--- app/script/service/borrower.js
+++ app/script/service/borrower.js
@@ -6,7 +6,7 @@ define([], function(config) {
         };
 
         //config.BORROWER_CONSOLE + '/hzq/project/:id'
-        var borrowerRes = $resource('script/data/data1.json', { id: "@id" }, { 'query': { isArray: false }, 'update': { method: 'PUT' } });
+        var borrowerRes = $resource('script/data/borrower-list.json', { id: "@id" }, { 'query': { isArray: false }, 'update': { method: 'PUT' } });
         return borrowerRes;
 
         // return {
