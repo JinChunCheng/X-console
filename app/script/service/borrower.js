376d9c95f59665a221e7b4c1e10d3d09e48f4431
尤海洋 <youhaiyang@iboxpay.com>
2016-08-18 12:59:50 +0800

    asset form files & dialog style

--- app/script/service/borrower.js
+++ app/script/service/borrower.js
@@ -7,49 +7,50 @@ define([], function(config) {
 
         //config.BORROWER_CONSOLE + '/hzq/project/:id'
         var borrowerRes = $resource('script/data/borrower-list.json', { id: "@id" }, { 'query': { isArray: false }, 'update': { method: 'PUT' } });
-        return borrowerRes;
+        //return borrowerRes;
 
-        // return {
-        //     /**
-        //      * get borrower list
-        //      * @param  {string} data 
-        //      */
-        //     query: function(data) {
-        //         return $http({
-        //                 url: 'script/data/data1.json',
-        //                 method: 'GET',
-        //                 data: data
-        //             })
-        //             .then(function(res) {
-        //                 return res ? res.data : serverErrorData;
-        //             }, function(res) {
-        //                 return $q.reject(res);
-        //             });
-        //     },
-        //     getAll: function(params) {
-        //         return borrowerRes.query(params).$promise
-        //             .then(function(res) {
-        //                 return res ? res : serverErrorData;
-        //             }, function(res) {
-        //                 return $q.reject(res);
-        //             });
-        //     },
-        //     save: function(params) {
-        //         return borrowerRes.save(params).$promise
-        //             .then(function(res) {
-        //                 return res ? res : serverErrorData;
-        //             }, function(res) {
-        //                 return $q.reject(res);
-        //             });
-        //     },
-        //     update: function(params) {
-        //         return borrowerRes.put(params).$promise
-        //             .then(function(res) {
-        //                 return res ? res : serverErrorData;
-        //             }, function(res) {
-        //                 return $q.reject(res);
-        //             });
-        //     }
-        // }
+        return {
+            resource: borrowerRes,
+            /**
+             * get borrower list
+             * @param  {string} data 
+             */
+            query: function(data) {
+                return $http({
+                        url: 'script/data/data1.json',
+                        method: 'GET',
+                        data: data
+                    })
+                    .then(function(res) {
+                        return res ? res.data : serverErrorData;
+                    }, function(res) {
+                        return $q.reject(res);
+                    });
+            },
+            getAll: function(params) {
+                return borrowerRes.query(params).$promise
+                    .then(function(res) {
+                        return res ? res : serverErrorData;
+                    }, function(res) {
+                        return $q.reject(res);
+                    });
+            },
+            save: function(params) {
+                return borrowerRes.save(params).$promise
+                    .then(function(res) {
+                        return res ? res : serverErrorData;
+                    }, function(res) {
+                        return $q.reject(res);
+                    });
+            },
+            update: function(params) {
+                return borrowerRes.put(params).$promise
+                    .then(function(res) {
+                        return res ? res : serverErrorData;
+                    }, function(res) {
+                        return $q.reject(res);
+                    });
+            }
+        }
     }]]
 });
