2d8bf6a4fb1e0af0b5317bae4eda67bc9a392b5e
尤海洋 <youhaiyang@iboxpay.com>
2016-08-19 19:16:54 +0800

    channel list add/edit

--- app/lib/ui-bootstrap/ui-bootstrap-tpls.js
+++ app/lib/ui-bootstrap/ui-bootstrap-tpls.js
@@ -4066,7 +4066,7 @@ angular.module("template/modal/backdrop.html", []).run(["$templateCache", functi
 angular.module("template/modal/window.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("template/modal/window.html",
     "<div tabindex=\"-1\" role=\"dialog\" class=\"modal fade\" ng-class=\"{in: animate}\" ng-style=\"{'z-index': 1050 + index*10, display: 'block'}\" ng-click=\"close($event)\">\n" +
-    "    <div class=\"modal-dialog\" ng-class=\"{'modal-sm': size == 'sm', 'modal-lg': size == 'lg'}\"><div class=\"modal-content\" modal-transclude></div></div>\n" +
+    "    <div class=\"modal-dialog\" ng-class=\"{'modal-sm': size == 'sm', 'modal-md': size == 'md', 'modal-lg': size == 'lg'}\"><div class=\"modal-content\" modal-transclude></div></div>\n" +
     "</div>");
 }]);
 
