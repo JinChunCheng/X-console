ce826cb5da53bcfbce023753714ab8c91805336e
周正 <zhouzheng@iboxpay.com>
2016-08-26 21:20:48 +0800

    batchUpLoad

--- app/script/controller/financial/full/full.js
+++ app/script/controller/financial/full/full.js
@@ -13,7 +13,54 @@ define([], function() {
 
         $scope.listView = {
             condition: angular.copy(defaultCondition),
-            table: null
+            table: null,
+            batchUpload: function() {
+                var selected = $scope.listView.table.bootstrapTable('getSelections');
+                if (!selected || selected.length === 0) {
+                    var text = "未选中行";
+                    $modal.open({
+                        templateUrl: 'view/shared/confirm.html',
+                        size: 'sm',
+                        controller: function($scope, $modalInstance) {
+                            $scope.confirmData = {
+                                text: text,
+                                processing: false
+                            };
+                            $scope.cancel = function() {
+                                $modalInstance.dismiss();
+                                return false;
+                            }
+                            $scope.ok = function() {
+                                $modalInstance.dismiss();
+                                return false;
+                            }
+                        }
+                    });
+                    return;
+                }
+                else {
+                    var text = "是否执行出款操作？";
+                    $modal.open({
+                        templateUrl: 'view/shared/confirm.html',
+                        size: 'sm',
+                        controller: function($scope, $modalInstance) {
+                            $scope.confirmData = {
+                                text: text,
+                                processing: false
+                            };
+                            $scope.cancel = function() {
+                                $modalInstance.dismiss();
+                                return false;
+                            }
+
+                            $scope.ok = function() {
+                                delUser(item.id, $scope, $modalInstance);
+                                return true;
+                            }
+                        }
+                    });
+                };
+            }
         };
 
         $scope.dateOptions = {
@@ -31,68 +78,22 @@ define([], function() {
         $scope.$on('$viewContentLoaded', function() {
             $scope.listView.table = $('#endBiddingCashTable');
         });
-
-
-                    var getData = function(params) {
-                //query: {where: JSON.stringify($scope.listVM.condition)}
-                borrowerService.resource.query({ where: JSON.stringify($scope.listView.condition) }).$promise.then(function(res) {
-                    //debugger
-                    $timeout(function() {
-                        res.data.items.forEach(function(item) {
-                            item.id = parseInt(Math.random() * 100);
-                        });
-                        res.data.items.sort(function(a, b) {
-                            return Math.random() > .5 ? -1 : 1;
-                        });
-                        params.success({
-                            total: res.data.paginate.totalCount,
-                            rows: res.data.items
-                        });
-                    }, 500);
-                });
-
-                //post: 
-                // var project = {};
-                // project.borrowerId = 1;
-                // project.contractTemplateId=1;
-                // project.projectName="console-前台添加";
-                // project.requestAmount=100000.00;
-                // project.repaymentType="IOP";
-                // project.duration=12;
-                // project.durationUnit="Y";
-                // project.periodCount=10;
-                // project.interestRate=0.8;
-                // project.interestRateTerm="Y";
-                // project.serviceFeeRate=0;
-                // project.serviceFeeRateTerm="Y";
-                // project.latePaymentFeeRateTerm="D";
-                // project.purpose="前端测试";
-                // project.mortgageFlag="N";
-                // project.mortgage="无";
-                // project.guaranteeFlag="N";
-                // project.guarantee="无";
-                // project.description="这是一个通过controller添加进来的project";
-                // project.biddingDeadline=new Date();
-                // project.biddingStartAmount=5000;
-                // project.biddingStepAmount=1000;
-                // project.biddingAmount=100000.00;
-                // project.status = "IRP";
-                // project.totalDays=100;
-                // project.totalInterest=100;
-                // project.totalServiceFee=0.0;
-                // project.debtStartDate=new Date();
-                // project.debtEndDate=new Date();
-                // project.principalPaid=0;
-                // project.PrincipalBalance=100;
-                // project.interestPaid=1;
-                // project.serviceFeePaid=0;
-                // project.memo="";
-                // project.creditChannelId=1;
-
-                // borrowerService.get(project).then(function(res) {
-                //     debugger
-                // });
-            };
+        var getData = function(params) {
+            borrowerService.resource.query({ where: JSON.stringify($scope.listView.condition) }).$promise.then(function(res) {
+                $timeout(function() {
+                    res.data.items.forEach(function(item) {
+                        item.id = parseInt(Math.random() * 100);
+                    });
+                    res.data.items.sort(function(a, b) {
+                        return Math.random() > .5 ? -1 : 1;
+                    });
+                    params.success({
+                        total: res.data.paginate.totalCount,
+                        rows: res.data.items
+                    });
+                }, 500);
+            });
+        };
 
         (function init() {
 
@@ -188,30 +189,7 @@ define([], function() {
             }
 
         })();
-        $scope.checkRow = function(e, value, row, index) {
-            var text = "是否执行出款操作？";
-            //text = JSON.stringify($scope.listVM.table.bootstrapTable('getSelections'));
-            $modal.open({
-                templateUrl: 'view/shared/confirm.html',
-                size: 'sm',
-                //backdrop: true,
-                controller: function($scope, $modalInstance) {
-                    $scope.confirmData = {
-                        text: text,
-                        processing: false
-                    };
-                    $scope.cancel = function() {
-                        $modalInstance.dismiss();
-                        return false;
-                    }
 
-                    $scope.ok = function() {
-                        delUser(item.id, $scope, $modalInstance);
-                        return true;
-                    }
-                }
-            });
-        };
 
         $scope.del = function() {
             console.log('del');
