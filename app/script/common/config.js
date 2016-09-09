69bbadfcd9cf1300992db5d0e020674512afaba5
尤海洋 <youhaiyang@iboxpay.com>
2016-09-09 16:28:56 +0800

    modify config path

--- app/script/common/config.js
+++ app/script/common/config.js
@@ -2,11 +2,11 @@ define(function() {
     return {
         BORROWER_CONSOLE: "http://172.21.1.205:8089/console-user-borrower",
         //BORROWER_CONSOLE: "http://172.21.20.8:8080",
-        ASSET_CONSOLE: 'http://172.21.1.205:8089/console-asset',
+        ASSET_CONSOLE: 'http://172.21.1.205:8088/console-asset',
         //ASSET_CONSOLE: 'http://172.21.22.31:8080',
-        PRODUCT_CONSOLE: 'http://172.21.1.205:8090/console-product-p2p',
+        PRODUCT_CONSOLE: 'http://172.21.1.205:8088/console-product-p2p',
         //PRODUCT_CONSOLE: 'http://172.21.22.31:8080',
-        RPOJECT_CONSOLE: 'http://172.21.1.205:8090/console-product-hzq',
+        RPOJECT_CONSOLE: 'http://172.21.1.205:8088/console-product-hzq',
         //RPOJECT_CONSOLE: 'http://172.21.20.17:8080',
         FILE_UPLOAD_CONSOLE: 'http://172.21.1.206',
         FILE_READ_CONSOLE: 'http://172.21.1.206',
@@ -15,13 +15,12 @@ define(function() {
         //OPERATION_CONSOLE: 'http://172.21.20.17:8083',
         INVESTOR_CONSOLE: 'http://172.21.1.205:8090/console-user-investor',
         CAPITAL_ACCOUNT_CONSOLE: 'http://172.21.1.205:8089/console-capital-account',
-        SALEPLATFORM_CONSOLE: 'http://172.21.1.205:8090/console-saleplatform',
         CASHOUT_CONSOLE: 'http://172.21.1.205:8089/console-cashout',
-        SALEPLATFORM_CONSOLE: 'http://172.21.1.205:8090/console-saleplatform',
-        RECHARGE_CONSOLE: 'http://172.21.1.205:8090/console-recharge',
-        PURCHASE_CONSOLE: 'http://172.21.1.205:8090/console-purchase',
+        SALEPLATFORM_CONSOLE: 'http://172.21.1.205:8087/console-saleplatform',
+        RECHARGE_CONSOLE: 'http://172.21.1.205:8087/console-recharge',
+        PURCHASE_CONSOLE: 'http://172.21.1.205:8087/console-purchase',
         REPORTS_CONSOLE: 'http://172.21.1.205:8090/console-reports',
         WITHDRAW_CONSOLE: 'http://172.21.1.205:8090/console-withdraw',
-        INVESTMENT_CONSOLE: 'http://172.21.1.205:8089/console-investment'
+        INVESTMENT_CONSOLE: 'http://172.21.1.205:8087/console-investment'
     }
 });
